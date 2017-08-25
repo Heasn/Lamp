8eee309742b962e7a7ebc4660149cf34dac0b82c
CHENBAIYU <chenbaiyu@moqihuyu.com>
2017-08-25 09:37:54 +0800

    添加项目文件。

---
+++ Lamp.Agent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lamp.Agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
